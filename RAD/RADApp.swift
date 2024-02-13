17ac19dd35942a50a5387fac1715e4b3f37847a0
Whisper010 <zinatullinlinar2003@gmail.com>
2024-02-13 10:53:50 +0100

    Initial Commit

---
+++ RAD/RADApp.swift
@@ -0,0 +1,17 @@
+//
+//  RADApp.swift
+//  RAD
+//
+//  Created by Linar Zinatullin on 13/02/24.
+//
+
+import SwiftUI
+
+@main
+struct RADApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
