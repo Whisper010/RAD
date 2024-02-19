e6c97cd61af9428d0743c7c567ed2a64098779d7
Whisper010 <zinatullinlinar2003@gmail.com>
2024-02-19 11:52:03 +0100

    Added @observable

--- RAD/ContentView.swift
+++ RAD/ContentView.swift
@@ -9,12 +9,39 @@ import SwiftUI
 import RealityKit
 import ARKit
 
+@Observable
+class ARLogic {
+    var isPlacementEnabled: Bool
+    
+    static let shared: ARLogic = ARLogic()
+    
+    private init() {
+        self.isPlacementEnabled = true
+    }
+}
+
 struct ContentView: View {
+    
+    var arLogic = ARLogic.shared
+    
     var body: some View {
         ZStack(alignment: .bottom){
             ARViewContainer()
+            SomeView(isPlacementEnabled: arLogic.isPlacementEnabled)
         }
         
+    }
+}
+
+struct SomeView : View {
+    
+     var isPlacementEnabled: Bool
+    
+    var body: some View{
+        if isPlacementEnabled{
+            EmptyView()
+        }
+        EmptyView()
     }
 }
 
