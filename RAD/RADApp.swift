7ef1cb1e043110c3696c7a35079b6de7f6f39400
Whisper010 <zinatullinlinar2003@gmail.com>
2024-02-19 10:40:27 +0100

    Adjust preffered scheme color

--- RAD/RADApp.swift
+++ RAD/RADApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct RADApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().preferredColorScheme(.dark)
         }
     }
 }
