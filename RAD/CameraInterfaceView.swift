3ac50eca769125ea7456f2626030e38a3569e789
Ali Asgari <ali8asgari@gmail.com>
2024-02-23 12:45:50 +0100

    Camera Interface View

---
+++ RAD/CameraInterfaceView.swift
@@ -0,0 +1,18 @@
+//
+//  CameraInterfaceView.swift
+//  RAD
+//
+//  Created by Ali Asgari on 23/02/24.
+//
+
+import SwiftUI
+
+struct CameraInterfaceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraInterfaceView()
+}
